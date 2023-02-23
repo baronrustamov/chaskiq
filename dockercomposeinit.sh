6caec4c83f36c47b779bc67cbc7598fe580a7e2b
baron <baronrustamov@gmail.com>
2023-02-23 14:53:46 +0300

    s

---
+++ dockercomposeinit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker-compose build
+docker-compose run rails bundle install
+docker-compose run rails yarn install
+docker-compose run rails db:setup 
+docker-compose run rails db:create
+docker-compose run rails db:schema:load
+docker-compose run rails db:seed
+docker-compose run rails rake admin_generator
+
+docker-compose up -d rails
+docker-compose up -d sidekiq
+docker-compose up -d
+
+echo 'done'
